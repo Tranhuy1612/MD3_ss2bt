6c6ee873a49c775d1607bd5ffe8358a20007437f
Tranhuy1612 <huytran1612@icloud.com>
2023-09-18 10:39:12 +0700

    MD3_ss2bt

--- src/bt1/Main.java
+++ src/bt1/Main.java
@@ -10,9 +10,9 @@ public class Main {
         System.out.print("Nhập giá trị cuối của khoảng: ");
         int cuoi = input.nextInt();
         int total = 0;
-        for (int number = dau; number <= cuoi; number++) {
-            if (number % 2 == 0) {
-               total = total + number;
+        for (int i = dau; i <= cuoi; i++) {
+            if (i % 2 == 0) {
+               total = total + i;
             }
         }
         System.out.println("Tổng các số chẵn trong khoảng từ " + dau + " đến " + cuoi + " là: " + total);
